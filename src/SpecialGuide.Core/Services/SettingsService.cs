b37b24c47c8115347175992fdaf7983b1b0c47cc
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-11 23:30:47 -0400

    Use configuration for settings

--- src/SpecialGuide.Core/Services/SettingsService.cs
+++ src/SpecialGuide.Core/Services/SettingsService.cs
@@ -1,34 +1,20 @@
 using System;
-using System.IO;
-using System.Text.Json;
+using Microsoft.Extensions.Options;
 using SpecialGuide.Core.Models;
 
 namespace SpecialGuide.Core.Services;
 
 public class SettingsService
 {
-    private readonly string _path;
-    public Settings Settings { get; private set; }
+    private readonly IOptionsMonitor<Settings> _settings;
+    public Settings Settings => _settings.CurrentValue;
     public string ApiKey => Settings.ApiKey;
 
-    public SettingsService()
-    {
-        var folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SpecialGuide");
-        _path = Path.Combine(folder, "settings.json");
-        if (File.Exists(_path))
-        {
-            Settings = JsonSerializer.Deserialize<Settings>(File.ReadAllText(_path)) ?? new Settings();
-        }
-        else
-        {
-            Settings = new Settings();
-        }
-    }
+    public event Action<Settings>? SettingsChanged;
 
-    public void Save()
+    public SettingsService(IOptionsMonitor<Settings> settings)
     {
-        var folder = Path.GetDirectoryName(_path)!;
-        Directory.CreateDirectory(folder);
-        File.WriteAllText(_path, JsonSerializer.Serialize(Settings, new JsonSerializerOptions { WriteIndented = true }));
+        _settings = settings;
+        _settings.OnChange(s => SettingsChanged?.Invoke(s));
     }
 }
