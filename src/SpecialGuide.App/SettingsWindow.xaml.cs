f3378ceb24ac8bd2218a06aec545c1c90433dbd7
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-13 11:38:13 -0400

    Improve settings service with fallback

--- src/SpecialGuide.App/SettingsWindow.xaml.cs
+++ src/SpecialGuide.App/SettingsWindow.xaml.cs
@@ -12,6 +12,7 @@ public partial class SettingsWindow : Window
         InitializeComponent();
         _settings = settings;
         DataContext = _settings.Settings;
+        _settings.SettingsChanged += s => Dispatcher.Invoke(() => DataContext = s);
     }
 
     private void OnSave(object sender, RoutedEventArgs e)
