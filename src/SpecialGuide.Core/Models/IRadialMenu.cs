1ff174305c9ba5c172b81e6b661aa1debe0b1c3e
alex.unnippillil <alex.unnippillil@hotmail.com>
2025-08-20 00:58:40 -0400

    Fix conflicts: removed problematic lines from 10 files

--- src/SpecialGuide.Core/Models/IRadialMenu.cs
+++ src/SpecialGuide.Core/Models/IRadialMenu.cs
@@ -1,10 +1,1 @@
-namespace SpecialGuide.Core.Models;
-
-public interface IRadialMenu
-{
-    event EventHandler? Cancelled;
-    void Populate(string[] suggestions);
-    void Show(double x, double y);
-    void ShowLoading(double x, double y);
-    void Hide();
-}
+﻿
